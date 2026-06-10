24ec16d9e9f5717626f1252029650b01163ae0e3
Miguel Alejandro <miguelalejandrosalcas@gmail.com>
2026-06-10 00:19:52 -0500

    Finalización lab abc integrantes - Rogelis - Henry - Diego - Elimer - Alejandro

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let cardsVocales = document.getElementById('btn-vocales');
 let cardsConsonantes = document.getElementById('btn-consonantes');
 let cards = document.querySelectorAll('.card');
+let cardFrente = document.querySelectorAll('.card-frente');
 let contadorSpan = document.getElementById('contador-span');
-let cardAbierta = false;
 let contador = 0;
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function girarCard(elemento) {
     elemento.classList.toggle('is-flipped');
+    cardFrente.forEach(cardFrente => {
+        cardFrente.style.backgroundColor = getRandomColor();
+    });
     
     if (elemento.classList.contains('is-flipped') && !elemento.dataset.contada) {
         contador++;
