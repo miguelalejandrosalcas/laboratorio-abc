79324c7ff2c35dcae943b626a8f24f2cc9ef37b2
Miguel Alejandro <miguelalejandrosalcas@gmail.com>
2026-06-09 23:38:25 -0500

    Merge rama alejandro

--- script.js
+++ script.js
@@ -5,24 +5,6 @@ let contadorSpan = document.getElementById('contador-span');
 let cardAbierta = false;
 let contador = 0;
 
-
-// letraClick.addEventListener('click', function() {
-//     const nuevaCard = document.createElement('div');
-//     nuevaCard.classList.add('card');
-//     nuevaCard.innerHTML = `
-//         <div class="card" style="width: 18rem;">
-//             <img src="..." class="card-img-top" alt="...">
-//             <div class="card-body">
-//                 <h5 class="card-title">Card title</h5>
-//                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-//                     card’s content.</p>
-//                 <a href="#" class="btn btn-primary">Go somewhere</a>
-//             </div>
-//         </div>
-//         `;
-// })
-
-
 function girarCard(elemento) {
     elemento.classList.toggle('is-flipped');
     
